bb500ba4ba576278d619d42222dad0b8b419dc99
Philip Foster <pfoster@Philips-MacBook-Pro.local>
2017-05-03 20:47:57 -0400

    Fixing whitespace

--- examples/s3/upload_file/upload.py
+++ examples/s3/upload_file/upload.py
@@ -9,19 +9,19 @@ def lambda_handler(event, context):
 	target_filename = "event.json"
 
 	# upload_filename is the remote location in S3
-    upload_filename = bucket_path + target_filename
+	upload_filename = bucket_path + target_filename
 
 	# output_path here is the location in /tmp to store the event
-    output_path = "/tmp/" + target_filename
-    
+	output_path = "/tmp/" + target_filename
+
 	# Read in the event object, convert it to json and dump it
 	# to the output_path in /tmp
-    output_filehandle = file(output_path, "w")
-    output_filehandle.writelines(json.dumps(event))
-    output_filehandle.close()
-    
-	# Setup the s3 client and upload the file
-    s3 = boto3.client("s3")
-    s3.upload_file(output_path, bucket_name, upload_filename)
+	output_filehandle = file(output_path, "w")
+	output_filehandle.writelines(json.dumps(event))
+	output_filehandle.close()
 
-    return 'saved to {0}'.format(upload_filename)
+	# Setup the s3 client and upload the file
+	s3 = boto3.client("s3")
+	s3.upload_file(output_path, bucket_name, upload_filename)
+
+	return 'saved to {0}'.format(upload_filename)
